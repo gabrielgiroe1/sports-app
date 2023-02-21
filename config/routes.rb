f68bfbdfe0fce753e1fde8ef5d603ab3e4b7710e
zaryabriaz <zaryabriaz@gmail.com>
2023-02-21 15:47:27 +0500

    Added edit method to update the user attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' }
   post 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
   get 'users/:id' => 'users#show', as: :user
 
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   post "/filters", to: "filters#index"
   get "/posts", to: "posts#index"
   get "/posts/new", to: "posts#new"
+  put "/users/:id", to: "users/registrations#update", as: "update_user_registration"
   resources :posts
   resources :users
 end
