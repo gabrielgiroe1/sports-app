5fe5c5d88b91c48ef5bbeb35451fb66defcf3ec6
Georgel Gabriel Giroe <gabriel.giroe@gmail.com>
2023-02-23 09:55:32 +0200

    Set weekly averages for the current user

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,5 @@
 class ReportsController < ApplicationController
   def weekly_averages
-    @weekly_averages = Post.weekly_averages
+    @weekly_averages = current_user.posts.weekly_averages
   end
 end
